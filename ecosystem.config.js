bf6f2ee2d1f94c140ae8b2c893776ef30a81ff0e
darlancaruso <devcaruso@gmail.com>
2021-03-20 14:25:53 -0300

    Ajustando comando

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,23 +1,16 @@
 module.exports = {
   apps: [
     {
-      script: "index.js",
-      watch: ".",
+      name: "app",
+      script: "./index.js",
+      instances: "max",
+      env: {
+        NODE_ENV: "development",
+      },
+      env_production: {
+        NODE_ENV: "production",
+        NODE_PORT: "80",
+      },
     },
   ],
-
-  deploy: {
-    production: {
-      user: "SSH_USERNAME",
-      host: "SSH_HOSTMACHINE",
-      ref: "origin/master",
-      repo: "GIT_REPOSITORY",
-      path: "DESTINATION_PATH",
-      "pre-deploy-local": "",
-      "post-deploy":
-        "npm install && pm2 reload ecosystem.config.js --env production",
-      "pre-setup": "",
-      port: "80"
-    },
-  },
 };
